25ca92cc775f65ae55dcfad879dbfc36d633300a
Kamesh Sampath <kameshsampath@hotmail.com>
2017-09-28 10:21:49 +0530

    Repo Init

---
+++ src/main/java/com/redhat/developers/SpringBootCheDemoApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.developers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCheDemoApplication.class, args);
+	}
+}
